8b150f2ff6b962a2fd35a2171518271407970390
gsz <gaoshengzhe1@gmail.com>
2026-01-09 00:45:44 +0900

    2

--- src/main/java/jp/asatex/ggsz/socialinsurance/entity/IncomeTaxBracket.java
+++ src/main/java/jp/asatex/ggsz/socialinsurance/entity/IncomeTaxBracket.java
@@ -2,6 +2,7 @@ package jp.asatex.ggsz.socialinsurance.entity;
 
 import lombok.Data;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
 import org.springframework.data.relational.core.mapping.Table;
 
 import java.math.BigDecimal;
@@ -13,13 +14,30 @@ public class IncomeTaxBracket {
     private Long id;
     private Integer minSalary;
     private Integer maxSalary;
+    @Column("dependents_0")
     private BigDecimal dependents0;
+    
+    @Column("dependents_1")
     private BigDecimal dependents1;
+    
+    @Column("dependents_2")
     private BigDecimal dependents2;
+    
+    @Column("dependents_3")
     private BigDecimal dependents3;
+    
+    @Column("dependents_4")
     private BigDecimal dependents4;
+    
+    @Column("dependents_5")
     private BigDecimal dependents5;
+    
+    @Column("dependents_6")
     private BigDecimal dependents6;
+    
+    @Column("dependents_7")
     private BigDecimal dependents7;
+    
+    @Column("tax_amount_col_b")
     private BigDecimal taxAmountColB;
 }
